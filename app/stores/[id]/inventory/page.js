8166095c0143f56af50b7f1116e7714475fd1204
tom-con <tconchie@gmail.com>
2024-03-21 12:39:54 +0600

    Adds a lot of design changes including navigation layout, and better inventory table

--- app/stores/[id]/inventory/page.js
+++ app/stores/[id]/inventory/page.js
@@ -1,4 +1,4 @@
-import OrderForm from "@/app/components/order-form";
+import { H1, P } from "@/app/components/design-system/typography";
 
 const { API_URL } = process.env;
 
@@ -8,20 +8,59 @@ export default async function Inventory({ params }) {
 	const { data: inventory } = await res.json();
 
 	return (
-		<div className="container mx-auto">
-			<h1 className="text-2xl font-bold mb-4">Inventory</h1>
-			<ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-				{inventory.map((item) => (
-					<li
-						key={item.id}
-						className="bg-white rounded-lg shadow-md p-4"
+		<div>
+			<div className="sm:flex sm:items-center">
+				<div className="sm:flex-auto">
+					<H1 text="Inventory" />
+					<P text="Your current product stock calculations." />
+				</div>
+				<div className="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
+					{/* <button
+						type="button"
+						className="block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
 					>
-						<h2 className="text-lg font-bold mb-2 text-black">{item.name}</h2>
-						<p className="text-gray-500">{item.amount}</p>
-						<OrderForm productId={item.product_id} storeId={params.id} />
-					</li>
-				))}
-			</ul>
+						Add user
+					</button> */}
+				</div>
+			</div>
+			<div>
+				<table className="min-w-full divide-y divide-gray-300">
+					<thead>
+						<tr>
+							<th scope="col" className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-200 sm:pl-0">
+								Product Name
+							</th>
+							<th
+								scope="col"
+								className="hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-200 sm:table-cell"
+							>
+								Amount In Stock
+							</th>
+							<th scope="col" className="relative py-3.5 pl-3 pr-4 sm:pr-0">
+								<span className="sr-only">Order More</span>
+							</th>
+						</tr>
+					</thead>
+					<tbody className="divide-y divide-gray-200 bg-white">
+						{inventory.map((item) => (
+							<tr key={item.product_id}>
+								<td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium bg-gray-900 text-white sm:pl-0">
+									{item.name}
+								</td>
+								<td className="hidden whitespace-nowrap px-3 py-4 text-sm bg-gray-900 text-white sm:table-cell">
+									{item.amount}
+								</td>
+								<td className="whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium  bg-gray-900 text-white sm:pr-0">
+									<a href="#" className="text-indigo-300 hover:text-indigo-500">
+										Order More<span className="sr-only">, {item.name}</span>
+									</a>
+								</td>
+							</tr>
+						))}
+					</tbody>
+				</table>
+			</div>
 		</div>
 	);
 }
+
