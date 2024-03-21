a213b71905ae84a380e3a1ce48fa3ee7c7dbe508
tom-con <tconchie@gmail.com>
2024-03-21 12:39:54 +0600

    Adds a lot of design changes including navigation layout, and better inventory table

---
+++ app/components/design-system/typography.jsx
@@ -0,0 +1,7 @@
+export const H1 = ({ text }) => {
+	return <h1 className="text-2xl font-semibold leading-6 text-white">{text}</h1>
+}
+
+export const P = ({ text }) => {
+	return <p className="mt-2 text-sm text-gray-100">{text}</p>
+}
