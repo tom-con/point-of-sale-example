a61e2b39fb4b01b88d57c02d7a08014e234049e4
tom-con <tconchie@gmail.com>
2024-03-21 13:38:23 +0600

    Adds loading functionality to button and svg for loader

--- app/components/button.jsx
+++ app/components/button.jsx
@@ -1,12 +1,13 @@
+import Loader from './loader';
 import React from 'react';
 
-const Button = ({ onClick, text }) => {
+const Button = ({ loading = true, onClick, text }) => {
 	return (
 		<button
 			className="bg-indigo-500 hover:bg-indigo-800 text-white font-bold py-2 px-4 rounded-md"
 			onClick={onClick}
 		>
-			{text}
+			{loading ? <Loader size="md" /> : text}
 		</button>
 	);
 };
